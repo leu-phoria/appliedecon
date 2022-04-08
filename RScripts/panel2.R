4063a78ee07abbad0b9c2645b2b259e6fa768e80
Leu Huang <leuhuang@googlemail.com>
2022-04-08 18:24:53 +0200

    add GMM

--- RScripts/panel2.R
+++ RScripts/panel2.R
@@ -15,7 +15,7 @@ summary(re)
 ##fe and re estimates and se are quite similar --> Hausman test
 ##no intercept in fe, but re and fe estim nevertheless the same
 
-phtest(inv~ value + capital, data =Grunfeld) 
+phtest(inv~ value + capital, data =Grunfeld)
 #p value 0.3119, cannot reject H0: random effects
 #with large datasets rather expected to reject H0
 
@@ -23,3 +23,13 @@ phtest(inv~ value + capital, data =Grunfeld)
 fd <- plm(inv~ value + capital, data =Grunfeld, model="fd")
 summary(fd)
 
+#data empluk
+data("EmplUK")
+summary(EmplUK)
+#plot(EmplUK$emp, type ='l')
+#panel GMM 
+dynpal <- pgmm(log(emp)~lag(log(emp), 1:2) + lag(log(wage), 0:1)+ log(capital)+ #1:2 first and second lag of emp, time 0 and t-1 logwage
+               lag(log(output), 0:1) | lag(log(emp),2:99), data=EmplUK, # | log(emp) --> instrument with all lags, that we didn't include
+               effect= "individual", model = "twosteps") 
+summary(dynpal)
+#interpretation is not so easy (?!) - some signif effects
